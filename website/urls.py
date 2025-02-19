f18729425a28eba45050a53b9b119698095f1e5d
Alex-SA1 <alexandrusamoila495@gmail.com>
2025-02-19 18:19:39 +0200

    implement backend for to-do list functionality (all CRUD operations)

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
-    path('main/', views.main_page, name='main')
+    path('main/', views.main_page, name='main'),
+    path('main/to-do-list/', TaskList.as_view(), name='to-do-list'),
+    path('main/to-do-list/task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('main/to-do-list/create-task/',
+         TaskCreate.as_view(), name='create-task'),
+    path('main/to-do-list/update-task/task/<int:pk>/',
+         TaskUpdate.as_view(), name='update-task'),
+    path('main/to-do-list/delete-task/task/<int:pk>/',
+         TaskDelete.as_view(), name='delete-task')
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import SignUpForm
+from django.views.generic.list import ListView
+from .models import Task
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 
 
 def home(request):
@@ -55,3 +62,126 @@ def register_user(request):
 @login_required(login_url='login')
 def main_page(request):
     return render(request, 'main.html', {})
+
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+    login_url = '/login'
+    redirect_field_name = 'next'
+
+    def get_queryset(self):
+        # self.request.user is the logged in user from the current session
+        return Task.objects.filter(user=self.request.user)
+
+
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+
+    login_url = '/login'
+    redirect_field_name = 'next'
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().user_id != request.user.id:
+            return redirect('/main/to-do-list')
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'task_form.html'
+
+    fields = ['title', 'description']
+    success_url = reverse_lazy('to-do-list')
+
+    login_url = '/login'
+    redirect_field_name = 'next'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+
+        form.fields['title'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Title',
+        })
+
+        form.fields['title'].label = ''
+
+        form.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Write a description for your task...'
+        })
+
+        form.fields['description'].label = ''
+
+        return form
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = ['title', 'description', 'is_complete']
+    success_url = reverse_lazy('to-do-list')
+
+    login_url = '/login'
+    redirect_field_name = 'next'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+
+        form.fields['title'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Title',
+        })
+
+        form.fields['title'].label = ''
+
+        form.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Write a description for your task...'
+        })
+
+        form.fields['description'].label = ''
+
+        form.fields['is_complete'].widget.attrs.update({
+            'class': 'form-check-input'
+        })
+
+        form.fields['is_complete'].label = 'Completed:'
+
+        return form
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().user_id != request.user.id:
+            return redirect('/main/to-do-list')
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('to-do-list')
+
+    login_url = '/login'
+    redirect_field_name = 'next'
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().user_id != request.user.id:
+            return redirect('/main/to-do-list')
+
+        return super().dispatch(request, *args, **kwargs)
